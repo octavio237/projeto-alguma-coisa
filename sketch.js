30364f528f1f11ba88f3204efcd20d2bb662206e
octavio237 <00001220370186sp@al.educacao.sp.gov.br>
2024-10-24 07:39:17 -0300

    botando um fundo

--- sketch.js
+++ sketch.js
@@ -2,17 +2,19 @@ var a =10;
 
 function setup() {
   createCanvas(1100, 1100);
+  background("black");
 }
 
 function draw() {
-  //background("black");
+  
   if (mouseIsPressed) {
     rect( mouseX, mouseY,25,25,40,40)
     fill("pink");
   } else {
     stroke("purple")
-    fill("pink");
+    fill("black");
   }
+  
 
 
 }
